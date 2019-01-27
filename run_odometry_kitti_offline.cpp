b7163d2594860e0417692c7777bb5d3f7a6f2efb
Yu Wang <wangyu@in.tum.de>
2019-01-27 18:14:16 +0100

    ready to test against KITTI stereo sequence.

--- run_odometry_kitti_offline.cpp
+++ run_odometry_kitti_offline.cpp
@@ -19,17 +19,18 @@
 #include "include/lm_optimizer.h"
 #include <se3.hpp>
 #include <typeinfo>
+#include <string>
 
 void load_gt_pose(const std::string& folder_name, std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& gt_poses);
 void load_data(const std::string& folder_name, std::vector<cv::Mat> &gray, int frame_id);
 void eval_pose(const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& gt_poses, const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& pred_poses);
-void plot(const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& gt_poses, const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& pred_poses);
+void save_txt(const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& gt_poses, const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& pred_poses);
 
 int main(){
 
   // TODO: hardcode camera params in depth_estimate, lm_optimizer, WarpPixel, ReprojectToCameraFrame
   // Kitti sequence00, calibration
-  unsigned int num_frames = 5;
+  unsigned int num_frames = 100; // 4000
   unsigned int num_pyramid = 4;
   std::string data_path = "../dataset/kitti";
   float fx = 718.856f; // in pixels
@@ -114,6 +115,7 @@ int main(){
     rela_pose = pose_estimator.Solve(pre_img_pyramid, pre_dep_pyramid, cur_img_pyramid);
     cur_pose = cur_pose * rela_pose.inverse();
     pred_poses.emplace_back(cur_pose.block<3,4>(0,0));
+    pose_estimator.Reset(init_relative_affine, 0.01f);
 
     // estimate depth & create depth-pyramid
     cv::Mat cur_left_val(cur_gray[0].rows, cur_gray[0].cols, CV_8U, init_val);
@@ -132,8 +134,10 @@ int main(){
     odometry::ImagePyramid pre_img_pyramid(4, cur_gray[0], false);
     cv::waitKey(100);
   }
-  std::cout << "sequence done!" << std::endl;
+  std::cout << "Sequence done! Evaluating translation error for the first 50 frames ..." << std::endl;
   eval_pose(gt_poses, pred_poses);
+  std::cout << "Saving poses for KITTI plot ..." << std::endl;
+  save_txt(gt_poses, pred_poses);
 
   return 0;
 }
@@ -189,13 +193,90 @@ void load_gt_pose(const std::string& folder_name, std::vector<Eigen::Matrix<floa
 void load_data(const std::string& folder_name, std::vector<cv::Mat> &gray, int frame_id){
   std::string left_path = folder_name + "/sequences/00/image_0/";
   std::string right_path = folder_name + "/sequences/00/image_1/";
+  std::string img_path0 = left_path + std::to_string(frame_id);
+  std::string img_path1 = right_path + std::to_string(frame_id);
+  cv::Mat gray_8u;
 
+  gray_8u = cv::imread(img_path0, cv::IMREAD_GRAYSCALE);
+  if (gray_8u.empty()){
+    std::cout << "read img failed." << std::endl;
+    std::exit(-1);
+  }
+  gray_8u.convertTo(gray[0], PixelType);
+
+  gray_8u = cv::imread(img_path1, cv::IMREAD_GRAYSCALE);
+  if (gray_8u.empty()){
+    std::cout << "read img failed." << std::endl;
+    std::exit(-1);
+  }
+  gray_8u.convertTo(gray[1], PixelType);
 }
 
 void eval_pose(const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& gt_poses, const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& pred_poses){
 
+  unsigned int num_frame = 50;
+  float trans_err;
+  float sum_err = 0.0f;
+  for (unsigned int i = 0; i < num_frame; i++){
+    trans_err = (pred_poses[i].block<3,1>(0,3) - gt_poses[i].block<3,1>(0,3)).norm();
+    sum_err += trans_err;
+    std::cout << "frame " << i << ": " << trans_err << std::endl;
+  }
+  std::cout << "avg error over " << num_frame << " frames: " << sum_err / float(num_frame) << std::endl;
 }
 
-void plot(const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& gt_poses, const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& pred_poses){
+void save_txt(const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& gt_poses, const std::vector<Eigen::Matrix<float, 3, 4, Eigen::RowMajor>>& pred_poses){
 
+  // for evaluation script, the size of gt_pose and pred_poses must be the same
+  if (gt_poses.size() != pred_poses.size()){
+    std::cout << "gt_pose.size() != pred_pose.size()" << std::endl;
+    exit(-1);
+  }
+
+  unsigned int num_frame = gt_poses.size();
+  std::string write_gt_file = "../dataset/kitti/devkit/data/odometry/poses/00.txt";
+  std::string write_pred_file = "../dataset/kitti/devkit/results/seq00/data/00.txt";
+  std::ofstream gt_file;
+  std::ofstream pred_file;
+  float tmp;
+  std::string gt_pose_line;
+  std::string pred_pose_line;
+  Eigen::Matrix<float, 3, 4, Eigen::RowMajor> gt_pose;
+  Eigen::Matrix<float, 3, 4, Eigen::RowMajor> pred_pose;
+
+  // open file & clear all data
+  gt_file.open(write_gt_file, std::ios::out | std::ios::trunc);
+  if (!gt_file.is_open()){
+    std::cout << "open gt write file failed: " << write_gt_file << std::endl;
+    exit(-1);
+  }
+  pred_file.open(write_pred_file, std::ios::out | std::ios::trunc);
+  if (!pred_file.is_open()){
+    std::cout << "open pred write file failed: " << write_pred_file << std::endl;
+    exit(-1);
+  }
+
+  // write poses to txt
+  for (unsigned int i = 0; i < num_frame; i++){
+    gt_pose = gt_poses[i];
+    pred_pose = pred_poses[i];
+    gt_pose_line = "";
+    pred_pose_line = "";
+    for (int param_id = 0; param_id < 12; param_id++){
+      tmp = gt_pose(param_id);
+      gt_pose_line += std::to_string(tmp);
+      tmp = pred_pose(param_id);
+      pred_pose_line += std::to_string(tmp);
+      if (param_id != 11){
+        gt_pose_line += " ";
+        pred_pose_line += " ";
+      }
+    }
+    gt_file << gt_pose_line;
+    pred_file << pred_pose_line;
+  }
+
+  gt_file.close();
+  pred_file.close();
+  std::cout << "save completed." << std::endl;
 }
